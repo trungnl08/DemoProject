913fb945c20742db45fcb66d7491202a4fd825db
Trungln08 <trung80.uet@gmail.com>
2022-06-24 18:11:06 +0700

    init

---
+++ DemoProject/CollectionView/Cell/CollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CollectionViewCell.swift
+//  DemoProject
+//
+//  Created by Frog Pepe on 24/06/2022.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
