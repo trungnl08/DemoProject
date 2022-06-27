b27d5bc61cba42cb0559f3baa2df9171093869b4
Trungln08 <trung80.uet@gmail.com>
2022-06-27 18:19:01 +0700

    update mapview

--- MapView.swift
+++ MapView.swift
@@ -7,17 +7,26 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
 class MapView: UIViewController {
 
     @IBOutlet weak var mapview: MKMapView!
+    let manager = CLLocationManager()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        let initialLocation = CLLocation(latitude: 21.030653, longitude: 105.847130)
-        mapview.centerToLocation(initialLocation)
+
 
 
         // Do any additional setup after loading the view.
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        manager.desiredAccuracy = kCLLocationAccuracyBest
+        manager.delegate = self
+        manager.requestWhenInUseAuthorization()
+        manager.startUpdatingLocation()
     }
     
 
@@ -32,15 +41,34 @@ class MapView: UIViewController {
     */
 
 }
-private extension MKMapView {
-  func centerToLocation(
-    _ location: CLLocation,
-    regionRadius: CLLocationDistance = 1000
-  ) {
-    let coordinateRegion = MKCoordinateRegion(
-      center: location.coordinate,
-      latitudinalMeters: regionRadius,
-      longitudinalMeters: regionRadius)
-    setRegion(coordinateRegion, animated: true)
-  }
+extension MapView: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.first {
+            manager.stopUpdatingLocation()
+            render(location)
+        }
+    }
+    func render(_ location: CLLocation){
+        let coordinate = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
+        let span = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
+        let region = MKCoordinateRegion(center: coordinate, span:span)
+        mapview.setRegion(region, animated: true)
+        let marker = MKPointAnnotation()
+        marker.coordinate = coordinate
+        mapview.addAnnotation(marker)
+    }
+    
 }
+
+//private extension MKMapView {
+//  func centerToLocation(
+//    _ location: CLLocation,
+//    regionRadius: CLLocationDistance = 1000
+//  ) {
+//    let coordinateRegion = MKCoordinateRegion(
+//      center: location.coordinate,
+//      latitudinalMeters: regionRadius,
+//      longitudinalMeters: regionRadius)
+//    setRegion(coordinateRegion, animated: true)
+//  }
+//}
