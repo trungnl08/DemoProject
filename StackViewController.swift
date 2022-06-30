29a0121330c9ba26e207bdc2907f4aeabda3818e
Trungln08 <trung80.uet@gmail.com>
2022-06-30 14:56:43 +0700

    up

--- StackViewController.swift
+++ StackViewController.swift
@@ -8,33 +8,31 @@
 import UIKit
 
 class StackViewController: UIViewController {
-
+    
     @IBOutlet weak var stackview: UIStackView!
     @IBAction func toggleVerti(_ sender: Any) {
         if(self.stackview.axis == .vertical){
-            
-        
-        self.stackview.axis = .horizontal
+            self.stackview.axis = .horizontal
         } else {
             self.stackview.axis = .vertical
         }
     }
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
     
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
